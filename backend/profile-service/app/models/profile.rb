e4cf252fa41dee1ea66236118beb2689a0e4de09
jdluques <juan.luque@utec.edu.pe>
2025-05-06 13:15:19 -0500

    feat: generate random nickname for new profiles

--- backend/profile-service/app/models/profile.rb
+++ backend/profile-service/app/models/profile.rb
@@ -1,3 +1,17 @@
 class Profile < ApplicationRecord
   belongs_to :institution
+  before_create :set_random_nickname
+
+  private
+
+  def set_random_nickname
+    self.nickname ||= generate_randon_nickname
+  end
+
+  def generate_randon_nickname
+    adjectives = %w[funky cool fast happy silly wild funny]
+    animals = %w[donkey panda lion tiger elephant giraffe penguin wolf]
+
+    "#{adjectives.sample}_#{animals.sample}"
+  end
 end
