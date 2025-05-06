b688848ad62b25caeb214b5da690d369d602761d
jdluques <juan.luque@utec.edu.pe>
2025-05-06 12:21:49 -0500

    feat(scaffold): add Profile model and controller with SQL constraints

---
+++ backend/profile-service/db/migrate/20250506171851_create_profiles.rb
@@ -0,0 +1,19 @@
+class CreateProfiles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :profiles do |t|
+      t.string :nickname, null: false
+      t.string :name
+      t.string :lastname
+      t.string :email, null: false
+      t.references :institution, null: false, foreign_key: true
+      t.date :birthday
+      t.string :gender
+      t.string :picture_url, null: false
+      t.integer :stars
+
+      t.timestamps
+    end
+    add_index :profiles, :nickname, unique: true
+    add_index :profiles, :email, unique: true
+  end
+end
