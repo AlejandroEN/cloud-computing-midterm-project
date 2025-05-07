a4ba5a773007841f29f6817bdcd88d1001893154
jdluques <juan.luque@utec.edu.pe>
2025-05-06 21:24:37 -0500

    refactor: change profiles endpoints to use /self

--- backend/profile-service/config/routes.rb
+++ backend/profile-service/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :presentation_cards
   resources :bookmarks
-  resources :profiles
+  resources :profiles, only: [:show, :create]
   resources :institutions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/profiles/self", to: "profiles#show_self"
+  patch "/profiles/self", to: "profiles#update", as: "update_profile"
+  delete "/profiles/self", to: "profiles#destroy", as: "destroy_profile"
 
 end
