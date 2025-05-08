750bb4321d704caa3c3140bd0962879b9e5b0d1e
jdluques <juan.luque@utec.edu.pe>
2025-05-07 20:33:25 -0500

    feat(server): feat(server): add Echo server setup with logger, recover, and CORS middleware

---
+++ backend/api-gateway/pkg/server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func NewServer() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	return e
+}
