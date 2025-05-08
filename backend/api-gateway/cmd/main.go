8db0f00683be3dd6ecb3b3ab8dd4cd16fc9053a0
jdluques <juan.luque@utec.edu.pe>
2025-05-07 21:01:15 -0500

    feat: add .env config in main

--- backend/api-gateway/cmd/main.go
+++ backend/api-gateway/cmd/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
+
 	"api-gateway/pkg/rerouting"
 	"api-gateway/pkg/server"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
 
 func main() {
 	e := server.NewServer()
 
 	rerouting.SetupRoutes(e)
-	
+
 	port := ":8080"
 	log.Printf("Starting Gateway API on port %s\n", port)
 	if err := e.Start(port); err != nil {
