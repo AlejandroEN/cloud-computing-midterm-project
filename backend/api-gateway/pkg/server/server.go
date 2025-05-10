5826a52b4bc8f9516b4d96d18fb27239433d5aee
jdluques <juan.luque@utec.edu.pe>
2025-05-09 20:49:28 -0500

    refactor: make server port configurable via environment variable

--- backend/api-gateway/pkg/server/server.go
+++ backend/api-gateway/pkg/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"log"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -18,7 +19,10 @@ func NewServer() *echo.Echo {
 }
 
 func StartServer(e *echo.Echo) {
-	port := ":8080"
+	port := ":" + os.Getenv("PORT")
+	if port == ":" {
+		port = ":8080"
+	}
 	log.Printf("Starting Gateway API on port %s\n", port)
 	if err := e.Start(port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
