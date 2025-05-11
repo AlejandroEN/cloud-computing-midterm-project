fee4daa9274affdf6ea7e9566914edd8c3bbddde
AlejandroEN <alejandroescobarnunez@gmail.com>
2025-05-11 04:39:00 -0500

    style: format code for consistency in FilterSection component

--- frontend/src/app/(home)/_components/FilterSection.tsx
+++ frontend/src/app/(home)/_components/FilterSection.tsx
@@ -5,15 +5,15 @@ import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 export default function FilterSection() {
-	const [minValue, setMinValue] = useState(0);
-	const [maxValue, setMaxValue] = useState(100);
-	const params = useSearchParams();
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(100);
+  const params = useSearchParams();
 
-	return (
-		<div className="flex flex-row gap-2 items-center">
-			<Input placeholder="Mín." />
-			a
-			<Input placeholder="Max." />
-		</div>
-	);
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Input placeholder="Mín." />
+      a
+      <Input placeholder="Max." />
+    </div>
+  );
 }
