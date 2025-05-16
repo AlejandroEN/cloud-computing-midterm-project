076ab911f51924e7fb6390a376f73c3562431036
AlejandroEN <alejandroescobarnunez@gmail.com>
2025-05-16 17:06:03 -0500

    refactor: clean up imports and formatting across multiple files

--- backend/post-service/src/app.module.ts
+++ backend/post-service/src/app.module.ts
@@ -1,26 +1,25 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { APP_INTERCEPTOR } from '@nestjs/core'; 
-
-import { PostsModule } from './posts/posts.module';
-import { ProfileInterceptor } from './common/profile.interceptor'; // Importar el interceptor
-import { S3Service } from './common/s3.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ProfileInterceptor } from "./common/profile.interceptor";
+import { S3Service } from "./common/s3.service";
+import { PostsModule } from "./posts/posts.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(
-      process.env.MONGODB_URI ?? 'mongodb://172.31.26.161:27017/postsdb'
-    ),
-    PostsModule
-  ],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: ProfileInterceptor, // Registrar el interceptor globalmente
-    },
-    S3Service, 
-  ],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		MongooseModule.forRoot(
+			process.env.MONGODB_URI ?? "mongodb://172.31.26.161:27017/postsdb"
+		),
+		PostsModule,
+	],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: ProfileInterceptor, // Registrar el interceptor globalmente
+		},
+		S3Service,
+	],
 })
-export class AppModule { }
+export class AppModule {}
